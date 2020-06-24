cb252eac570d3fec48dca9f95852858922bab4ab
davidboren <dboren@ucla.edu>
2020-06-23 19:21:32 -0700

    Artifact root

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-DB_URI=postgresql+psycopg2://$MLFLOW_USERNAME:$MLFLOW_PASSWORD@$MLFLOW_SERVER/$MLFLOW_DATABASE
+DB_URI=postgresql+psycopg2://$MLFLOW_USERNAME:$MLFLOW_PASSWORD@$MLFLOW_RDS_ENDPOINT/$MLFLOW_DATABASE
 # mlflow db upgrade $DB_URI
-mlflow server --backend-store-uri $DB_URI
+mlflow server --default-artifact-root $MLFLOW_ARTIFACT_ROOT --backend-store-uri $DB_URI
