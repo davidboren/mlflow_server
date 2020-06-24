8035d791ac0ddb840b47585cf2edab361983bf03
davidboren <dboren@ucla.edu>
2020-06-23 19:48:11 -0700

    Host

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 DB_URI=postgresql+psycopg2://$MLFLOW_USERNAME:$MLFLOW_PASSWORD@$MLFLOW_RDS_ENDPOINT/$MLFLOW_DATABASE
 # mlflow db upgrade $DB_URI
-mlflow server --default-artifact-root $MLFLOW_ARTIFACT_ROOT --backend-store-uri $DB_URI
+mlflow server --host --default-artifact-root $MLFLOW_ARTIFACT_ROOT --backend-store-uri $DB_URI
